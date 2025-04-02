2a5c481289cab82beb94db7d50ca2ce10ba8d773
S. Pratham <educate.pratham@gmail.com>
2025-04-02 20:41:06 +0530

    Create data_module.py

---
+++ api/data_module.py
@@ -0,0 +1,7 @@
+# data_module.py
+
+documents = [
+    "Operating the Climate Control System  Your Googlecar has a climate control system...",
+    "Your Googlecar has a large touchscreen display that provides access to a variety of features...",
+    "Shifting Gears Your Googlecar has an automatic transmission..."
+]
